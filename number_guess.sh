f82e2b7f0ca542e23824e05b3452875127e316be
Erik Cisneros <eca.experimental@gmail.com>
2024-05-19 18:50:31 -0400

    chore: Outline script logic flow with comments

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+# Generate random number between 1 and 1000
+
+# Prompt user for username:
+
+# Search user in database
+
+# If the user is a returning player output greeting 1
+
+# Else (user is a new player), output greeting 2
+ # Insert player into database
+
+# Prompt user for guess in this current game:
+
+# Check if guess is numeric
+# If number is not numeric, ask for another guess
+
+# Otherwise continue with numeric comparison
+  # If number is less than guess, ask for another guess
+
+  # Else if number is greater than guess, ask for another guess
+
+  # Otherwise number is a match,
+  # Congratulate player and finish running
