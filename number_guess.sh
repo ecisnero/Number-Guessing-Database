b5dd1773906b033ddc14b118cbe9a4fa29c50e5d
Erik Cisneros <eca.experimental@gmail.com>
2024-05-19 20:21:03 -0400

    feat: Update user stats

--- number_guess.sh
+++ number_guess.sh
@@ -56,6 +56,18 @@ guess() {
     else
     # Congratulate player and finish running
       echo You guessed it in $guess_count tries. The secret number was $random_number. Nice job!
+
+      # Update Stats
+      user_results=$($PSQL "SELECT username, games_played, best_game FROM user_stats WHERE username='$username';")
+      echo $user_results | while IFS='|' read username games_played best_game
+      do
+        games_count=$(( $games_played + 1 ))
+        games_played_update_result=$($PSQL "UPDATE user_stats SET games_played=$games_count WHERE username='$username';")
+        if [[ -z $best_game || $guess_count -lt $best_game ]]
+        then
+          best_game_update_result=$($PSQL "UPDATE user_stats SET best_game=$guess_count WHERE username='$username';")
+        fi
+      done
     fi
   fi
 }
